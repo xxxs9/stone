72c212c41d3e14c912acd23f647a37da59ea12b6
Oliver <694052985@qq.com>
2019-04-07 00:11:49 +0800

    采购部门  完善主页显示的表格，尚未进行权限分配

--- src/main/webapp/layuicms/page/main1.js
+++ src/main/webapp/layuicms/page/main1.js
@@ -30,7 +30,7 @@ layui.config({
         $api.selectRole(null,function (res) {
             var data = res.data;
                 for(var i=0;i<data.length;i++) {
-                    if (data[i] = 'admin') {
+                    if (data[i] = '采购部门员工') {
                         //超级管理员，显示所有表格
                         $('#messageShow').show();
                         $('#supplierShow').show();
@@ -47,9 +47,9 @@ layui.config({
                                 , url: $tool.getContext() + 'allRole/notify.do' //数据接口
                                 , method: 'post'
                                 , cols: [[ //表头
-                                    {field: 'receiverId', title: '发件人', width: 150}
+                                    {field: 'receiverId', title: '收件人', width: 150}
                                     , {field: 'sendTime', title: '发送时间', width: 200}
-                                    , {field: 'content', title: '邮件内容'}
+                                    , {field: 'content', title: '邮件内容',width:790}
                                 ]]
                                 , done: function (res, curr) {//请求完毕后的回调
                                     // 如果是异步请求数据方式，res即为你接口返回的信息.curr：当前页码
@@ -68,9 +68,9 @@ layui.config({
                                 , cols: [[ //表头
                                     {field: 'supplierName', title: '供应商', width: 200}
                                     , {field: 'goodsName', title: '货物名称', width: 150}
-                                    , {field: 'goodsPrice', title: '货物价格', width: 100}
+                                    , {field: 'goodsPrice', title: '货物单格', width: 100}
                                     , {field: 'phone', title: '供应商电话', width: 200}
-                                    , {field: 'email', title: '供应商邮箱', width: 200}
+                                    , {field: 'email', title: '供应商邮箱', width: 488}
                                 ]]
                                 , done: function (res, curr) {//请求完毕后的回调
                                     // 如果是异步请求数据方式，res即为你接口返回的信息.curr：当前页码
@@ -90,8 +90,8 @@ layui.config({
                                     {field: 'orderId', title: '订单单号', width: 150}
                                     , {field: 'applyUser', title: '销售员', width: 150}
                                     , {field: 'customer', title: '客户名', width: 150}
-                                    , {field: 'productId', title: '商品名称', width: 150}
-                                    , {field: 'orderTime', title: '购买日期', width: 200}
+                                    , {field: 'saleName', title: '商品名称', width: 150}
+                                    , {field: 'orderTime', title: '购买日期', width: 540}
                                 ]]
                                 , done: function (res, curr) {//请求完毕后的回调
                                     // 如果是异步请求数据方式，res即为你接口返回的信息.curr：当前页码
@@ -110,9 +110,9 @@ layui.config({
                                 , cols: [[ //表头
                                     {field: 'orderNumber', title: '订单编号', width: 150}
                                     , {field: 'orderAuditUser', title: '采购员', width: 150}
-                                    , {field: 'goodsId', title: '采购商品', width: 150}
+                                    , {field: 'goodsName', title: '采购商品', width: 150}
                                     , {field: 'goodsNumber', title: '采购数量', width: 150}
-                                    , {field: 'applyTime', title: '采购时间'}
+                                    , {field: 'applyTime', title: '采购时间',width:540}
                                 ]]
                                 , done: function (res, curr) {//请求完毕后的回调
                                     // 如果是异步请求数据方式，res即为你接口返回的信息.curr：当前页码
@@ -129,10 +129,10 @@ layui.config({
                                 , url: $tool.getContext() + 'allRole/depot.do' //数据接口
                                 , method: 'post'
                                 , cols: [[ //表头
-                                    {field: 'depotType', title: '状态', width: 150}
-                                    , {field: 'depotName', title: '货物名称', width: 150}
-                                    , {field: 'depotNumber', title: '货物数量', width: 150}
-                                    , {field: 'outInTime', title: '更新时间', width: 200}
+                                    {field: 'type', title: '状态', width: 200}
+                                    , {field: 'depotName', title: '货物名称', width: 200}
+                                    , {field: 'depotNumber', title: '货物数量', width: 200}
+                                    , {field: 'saleableNumber', title: '可销售数量', width: 540}
                                 ]]
                                 , done: function (res, curr) {//请求完毕后的回调
                                     // 如果是异步请求数据方式，res即为你接口返回的信息.curr：当前页码
@@ -149,10 +149,10 @@ layui.config({
                                 , url: $tool.getContext() + 'allRole/product.do' //数据接口
                                 , method: 'post'
                                 , cols: [[ //表头
-                                    {field: 'productName', title: '产品名称', width: 150}
-                                    , {field: 'productNumber', title: '生产数量', width: 150}
-                                    , {field: 'productState', title: '生产状态', width: 150}
-                                    , {field: 'canSold', title: '来源', width: 150}
+                                    {field: 'productName', title: '产品名称', width: 200}
+                                    , {field: 'productNumber', title: '生产数量', width: 200}
+                                    , {field: 'productState', title: '生产状态', width: 200,templet:"#productState"}
+                                    , {field: 'canSold', title: '来源', width: 540}
                                 ]]
                                 , done: function (res, curr) {//请求完毕后的回调
                                     // 如果是异步请求数据方式，res即为你接口返回的信息.curr：当前页码
@@ -169,10 +169,10 @@ layui.config({
                                 , url: $tool.getContext() + 'allRole/finance.do' //数据接口
                                 , method: 'post'
                                 , cols: [[ //表头
-                                    {field: 'financeApplyUser', title: '申请人', width: 150}
-                                    , {field: 'applyMoney', title: '申请金额', width: 150}
-                                    , {field: 'financeApplyState', title: '审核状态', width: 150, templet: "#trans"}
-                                    , {field: 'financeApplyTime', title: '申请时间', width: 200}
+                                    {field: 'financeApplyUser', title: '申请人', width: 200}
+                                    , {field: 'applyMoney', title: '申请金额', width: 200}
+                                    , {field: 'financeApplyState', title: '审核状态', width: 200, templet: "#trans"}
+                                    , {field: 'financeApplyTime', title: '申请时间', width: 540}
                                 ]]
                                 , done: function (res, curr) {//请求完毕后的回调
                                     // 如果是异步请求数据方式，res即为你接口返回的信息.curr：当前页码
@@ -182,7 +182,7 @@ layui.config({
 
                         financeTable();
 
-                    } else
+                    } /*else
                     if (data[i] = '采购部门员工') {
                         //采购部门，只显示采购部和通知部内容
                         $('#messageShow').show();
@@ -227,7 +227,7 @@ layui.config({
                         }
 
                         proOrderTable();
-                    }
+                    }*/
 
                 }
         });
