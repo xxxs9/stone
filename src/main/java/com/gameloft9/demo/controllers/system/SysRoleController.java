f4a5912ac44a5c256ff1853a3734c60050f2d042
afabao <421377215@qq.com>
2019-04-08 10:45:32 +0800

    update index2

--- src/main/java/com/gameloft9/demo/controllers/system/SysRoleController.java
+++ src/main/java/com/gameloft9/demo/controllers/system/SysRoleController.java
@@ -14,9 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import javax.servlet.http.HttpServletRequest;
 import java.util.Collection;
-import java.util.List;
 
 /**
  * 系统角色controller
